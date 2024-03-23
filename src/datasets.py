c9a0d788035987481e5276b18f8569dbb266e10a
TheCarmelGuy <komelm13@gmail.com>
2024-03-23 16:42:48 -0400

    typosg

--- src/datasets.py
+++ src/datasets.py
@@ -2,16 +2,13 @@ import os
 import torch
 import pandas as pd
 from skimage import io, transform
-import numpy as np
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from torchvision import transforms, utils
-from skimage.util import crop
 class KittiDataset(Dataset):
     def __init__(self,csv_file, root_dir, transform=None):
         self.root_dir = root_dir
         self.dataset_csv = pd.read_csv(os.path.join(self.root_dir,csv_file))    
-        self.tranform = transform
-        self.tranform = transform
+        self.transform = transform
 
     def __len__(self):
         return len(self.dataset_csv)
@@ -26,9 +23,9 @@ class KittiDataset(Dataset):
         return_image = torch.Tensor(io.imread(image_name)/255).permute(2,0,1) #normalize image from 0-1 off the bat 
         return_seg = torch.Tensor(io.imread(seg_filename))
         return_seg = return_seg.reshape(1,return_seg.shape[0], return_seg.shape[1]) #have to add make this 1xNxN to make torch.Resize() happy
-        if(self.tranform):
-            return_image = self.tranform(return_image)
-            return_seg = self.tranform(return_seg)
+        if(self.transform):
+            return_image = self.transform(return_image)
+            return_seg = self.transform(return_seg)
         
         return return_image,return_seg
     
