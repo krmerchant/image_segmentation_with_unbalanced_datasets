33b7c2f819200da96055921e42890d764d4ae0b3
TheCarmelGuy <komelm13@gmail.com>
2024-03-23 15:58:05 -0400

    sucessfully able to center crop images

--- src/datasets.py
+++ src/datasets.py
@@ -5,11 +5,12 @@ from skimage import io, transform
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
-
+from skimage.util import crop
 class KittiDataset(Dataset):
     def __init__(self,csv_file, root_dir, transform=None):
         self.root_dir = root_dir
         self.dataset_csv = pd.read_csv(os.path.join(self.root_dir,csv_file))    
+        self.tranform = transform
         self.tranform = transform
 
     def __len__(self):
@@ -22,16 +23,13 @@ class KittiDataset(Dataset):
         # pull index from csv file
         image_name = os.path.join(self.root_dir, "image_2/", self.dataset_csv.iloc[index,0]);
         seg_filename = os.path.join(self.root_dir, "semantic/", self.dataset_csv.iloc[index,0]);
-        seg_rgb_filename = os.path.join(self.root_dir, "semantic_rgb/", self.dataset_csv.iloc[index,0]);
-        image = io.imread(image_name)
-        seg_image = io.imread(seg_filename)
-        seg_rgb_image = io.imread(seg_rgb_filename)
-        sample = {'image': image, 'seg_mask':seg_image, 'seg_rgb_mask':seg_rgb_image}
-
+        return_image = torch.Tensor(io.imread(image_name)/255).permute(2,0,1) #normalize image from 0-1 off the bat 
+        return_seg = torch.Tensor(io.imread(seg_filename))
         if(self.tranform):
-            sample = self.tranform(sample)
+            return_image = self.tranform(return_image)
+            return_seg = self.tranform(return_seg)
         
-        return sample
+        return return_image,return_seg
     
 
 
--- src/test_datasets.py
+++ src/test_datasets.py
@@ -1,18 +1,38 @@
 from datasets import KittiDataset
 import matplotlib.pyplot as plt
-
-
+from torch.utils.data import DataLoader
+from torchvision import transforms, utils
+from PIL import Image
 def main():
-    kitti = KittiDataset('dataset.csv', '../data/kitti_semantic/training')
+   # kitti = KittiDataset('dataset.csv', '../data/kitti_semantic/training', transforms.Compose([
+   #     transforms.Resize((1200,1200))
+   # ]))
+    kitti = KittiDataset('dataset.csv', '../data/kitti_semantic/training', transforms.Compose([
+         transforms.CenterCrop(200)
+     ]))
+    im = 0; 
+    
+    for(i ,( image, seg)) in enumerate(kitti):
+        print(i)
+        print(image.shape)
+        print(seg.shape)
+        if i==10:
+            fig,ax = plt.subplots(1,2)
+            numpy_image = image.permute(1,2,0).numpy()
+            numpy_seg = seg.numpy()
+            ax[0].imshow((numpy_image))
+            ax[1].imshow(numpy_seg)
 
-    for(i , sample) in enumerate(kitti):
-        if i == 3:
-            fig, ax = plt.subplots(1,3)
-            ax[0].imshow(sample['image'])
-            ax[1].imshow(sample['seg_mask'])
-            ax[2].imshow(sample['seg_rgb_mask'])
 
+   
+    data_loader = DataLoader(kitti, batch_size=32, shuffle=False)
+    for batch in data_loader:
+       print("Got a Batch")
+       inputs, labels = batch  # Assuming each batch returns inputs and labels
+       print(inputs.shape)
+       print(labels.shape)
+      # Your training/evaluation loop
+    print("Sucessfully interated through batches!")
     plt.show()
-
 if __name__ == "__main__":
     main()
