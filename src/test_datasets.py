3671ce9aac488d0058b2492adbdca7afbbd13b07
TheCarmelGuy <komelm13@gmail.com>
2024-03-24 22:10:55 -0400

    linting

--- src/test_datasets.py
+++ src/test_datasets.py
@@ -4,40 +4,41 @@ from torch.utils.data import DataLoader
 from torchvision import transforms, utils
 from PIL import Image
 import numpy as np
+
+
 def main():
     kitti = KittiDataset('dataset.csv', '../data/kitti_semantic/training', transforms.Compose([
-        transforms.Resize((572,572))
+        transforms.Resize((572, 572))
     ]))
    # kitti = KittiDataset('dataset.csv', '../data/kitti_semantic/training', transforms.Compose([
    #      transforms.CenterCrop(350)
    #  ]))
-    
-    for(i ,( image, seg)) in enumerate(kitti):
+
+    for (i, (image, seg)) in enumerate(kitti):
         print(i)
-        #print(image.shape)
-        #print(seg.shape)
-        if i==100:
-            
-            numpy_image = image.permute(1,2,0).numpy()
+        # print(image.shape)
+        # print(seg.shape)
+        if i == 100:
+
+            numpy_image = image.permute(1, 2, 0).numpy()
             numpy_seg = seg.reshape(seg.shape[1], seg.shape[2]).numpy()
             print(numpy_seg.shape)
-            fig,ax = plt.subplots(1,2)
+            fig, ax = plt.subplots(1, 2)
             ax[0].imshow((numpy_image))
             seg_map = ax[1].imshow(numpy_seg)
             fig.colorbar(seg_map)
             print(numpy_seg)
 
-
-
-   
     data_loader = DataLoader(kitti, batch_size=32, shuffle=True)
     for batch in data_loader:
-       print("Got a Batch")
-       inputs, labels = batch  # Assuming each batch returns inputs and labels
-       print(inputs.shape)
-       print(labels.shape)
-      # Your training/evaluation loop
+        print("Got a Batch")
+        inputs, labels = batch  # Assuming each batch returns inputs and labels
+        print(inputs.shape)
+        print(labels.shape)
+       # Your training/evaluation loop
     print("Sucessfully interated through batches!")
     plt.show()
+
+
 if __name__ == "__main__":
     main()
