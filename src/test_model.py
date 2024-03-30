7dc816a7e95ad5a348564e0438cbcabb567e7572
TheCarmelGuy <komelm13@gmail.com>
2024-03-30 06:14:18 -0400

    fixed model and unit tests -- crop and cat along dim 1

--- src/test_model.py
+++ src/test_model.py
@@ -2,22 +2,23 @@ from models import UNet
 import unittest
 from datasets import KittiDataset
 from torchvision import transforms, utils
-
+import torch
 
 class TestUNetNetwork(unittest.TestCase):
 
     def test_construction(self):
         unet = UNet(50)
 
-    def test_pass_sample(self):
-        kitti = KittiDataset('dataset.csv', '../data/kitti_semantic/training', transforms.Compose([
-            transforms.Resize((1024, 1024))
-        ]))
-        [images, seg] = kitti[0]
-        unet = UNet(37)
+  
+    def test_pass_batch(self):
+        """test to make sure we can pass in a batch and get an the expected value"""
+        images = torch.ones(2,3,512,512)
+        unet = UNet(37,debug=False)
+        
         output = unet(images)
-        c,w,h = output.shape
-        self.assertEqual([37,1024, 1024], [c,w,h])
+        b,c,w,h = output.shape
+        self.assertEqual([2,37,512, 512], [b,c,w,h])
+
 
 
 if __name__ == '__main__':
