959ec4a81be522506fcb917f433ef6af175ddbff
TheCarmelGuy <komelm13@gmail.com>
2024-03-30 16:17:36 -0400

    refactor unet class

--- src/models.py
+++ src/models.py
@@ -4,10 +4,19 @@ from torchvision import models
 from torch.nn.functional import relu
 
 
-def center_crop(x, N):
-    """Extract Center"""
-    c, w, h = x.shape
-    return x[:, w//2-(N//2):w//2+(N//2), h//2-(N//2):h//2+(N//2)]
+class DoubleConv(nn.Module):
+    def __init__(self, input_channels, out_channels, debug=False):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(input_channels, out_channels, kernel_size=3,padding=1),
+            nn.ReLU(), 
+            nn.Conv2d(out_channels, out_channels, kernel_size=3,padding=1),
+            nn.ReLU() ,
+        )
+    def forward(self,x):
+        """forwared"""
+        output = self.double_conv(x)
+        return output
 
 
 class UNet(nn.Module):
@@ -15,40 +24,32 @@ class UNet(nn.Module):
         super().__init__()
         self.debug = debug
         self.number_classes = number_classes
-        self.encoder1_1 = nn.Conv2d(3, 64, kernel_size=3, padding=1) #3x572x572
-        self.encoder1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1) #64x572x572 (this concats accross)
+        self.encoder1 = DoubleConv(3,64) 
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) #64x286x286
 
-        self.encoder2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1) #128x256x256
-        self.encoder2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1) #128x256x256
+
+        self.encoder2 = DoubleConv(64,128) 
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) #128x128x128
 
-        self.encoder3_1 = nn.Conv2d(128, 256, kernel_size=3,padding=1) #256X128x128
-        self.encoder3_2 = nn.Conv2d(256, 256, kernel_size=3,padding=1) #256X128x128
+        self.encoder3 = DoubleConv(128, 256) #256X128x128
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)  #256x64x64
 
-        self.encoder4_1 = nn.Conv2d(256, 512, kernel_size=3,padding=1) #512x64x64
-        self.encoder4_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1) #512x64x64
+        self.encoder4 = DoubleConv(256, 512) #512x64x64
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) #512x32x32
 
-        self.encoder5_1 = nn.Conv2d(512, 1024, kernel_size=3,padding=1) #1024x32x32
-        self.encoder5_2 = nn.Conv2d(1024, 1024, kernel_size=3,padding=1) #1024x64x64
+        self.encoder5 = DoubleConv(512, 1024) #512x64x64
 
         self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.decoder1_1 = nn.Conv2d(1024, 512, kernel_size=3,padding=1)
-        self.decoder1_2 = nn.Conv2d(512, 512, kernel_size=3,padding=1)
+        self.decoder1 = DoubleConv(1024, 512)
 
         self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.decoder2_1 = nn.Conv2d(512, 256, kernel_size=3,padding=1)
-        self.decoder2_2 = nn.Conv2d(256, 256, kernel_size=3,padding=1)
+        self.decoder2 =  DoubleConv(512, 256)
 
         self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.decoder3_1 = nn.Conv2d(256, 128, kernel_size=3,padding=1)
-        self.decoder3_2 = nn.Conv2d(128, 128, kernel_size=3,padding=1)
+        self.decoder3 = DoubleConv(256, 128)
 
         self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.decoder4_1 = nn.Conv2d(128, 64, kernel_size=3,padding=1)
-        self.decoder4_2 = nn.Conv2d(64, 64, kernel_size=3,padding=1)
+        self.decoder4 = DoubleConv(128, 64)
 
         self.seg_out = nn.Conv2d(
             64, number_classes, kernel_size=1)  # 1x1 convolution
@@ -60,63 +61,47 @@ class UNet(nn.Module):
         """ forward pass of network"""
 
         # encoders
-        x_e11 = relu(self.encoder1_1(x))
-        if self.debug: print(f"{x_e11.shape=}")
-        x_e12 = relu(self.encoder1_2(x_e11))
-        if self.debug: print(f"{x_e12.shape=}")
-        x_pool1 = self.pool1(x_e12)
+        x_e1 = self.encoder1(x) 
+        x_pool1 = self.pool1(x_e1)
         if self.debug: print(f"{x_pool1.shape=}")
-        x_e21 = relu(self.encoder2_1(x_pool1))
-        if self.debug: print(f"{x_e21.shape=}")
-        x_e22 = relu(self.encoder2_2(x_e21))
-        if self.debug: print(f"{x_e22.shape=}")
-        x_pool2 = self.pool2(x_e22)
+        
+        x_e2 = self.encoder2(x_pool1)
+        if self.debug: print(f"{x_e2.shape=}")
+        x_pool2 = self.pool2(x_e2)
         if self.debug: print(f"{x_pool2.shape=}")
         
-        x_e31 = relu(self.encoder3_1(x_pool2))
-        if self.debug: print(f"{x_e31.shape=}")
-        x_e32 = relu(self.encoder3_2(x_e31))
-        if self.debug: print(f"{x_e32.shape=}")
-        x_pool3 = self.pool3(x_e32)
+        x_e3 = self.encoder3(x_pool2)
+        if self.debug: print(f"{x_e3.shape=}")
+        x_pool3 = self.pool3(x_e3)
         if self.debug: print(f"{x_pool3.shape=}")
         
-        x_e41 = relu(self.encoder4_1(x_pool3))
-        x_e42 = relu(self.encoder4_2(x_e41))
-        x_pool4 = self.pool4(x_e42)
+        x_e4 = self.encoder4(x_pool3) 
+        x_pool4 = self.pool4(x_e4)
 
-        x_e51 = relu(self.encoder5_1(x_pool4))
-        x_e52 = relu(self.encoder5_2(x_e51))
+        x_e5 = self.encoder5(x_pool4) 
+        # decoderrelu(self.encoder5_1(x_pool4))
 
-        # decoder
 
-        if self.debug: print(f"{x_e52.shape=}")
-        xup1 = self.upconv1(x_e52)
+        if self.debug: print(f"{x_e5.shape=}")
+        xup1 = self.upconv1(x_e5)
         if self.debug: print(f"{xup1.shape=}")
-        xcc1 = torch.cat([xup1, x_e42], dim=1)
+        xcc1 = torch.cat([xup1, x_e4], dim=1)
         if self.debug: print(f"{xcc1.shape=}")
-        xd11 = relu(self.decoder1_1(xcc1))
-        if self.debug: print(f"{xd11.shape=}")
-        xd12 = relu(self.decoder1_2(xd11))
-        if self.debug: print(f"{xd12.shape=}")
+        xd1 = self.decoder1(xcc1); 
+        if self.debug: print(f"{xd1.shape=}")
 
-        xup2 = self.upconv2(xd12)
-        if self.debug: print(f"{xup2.shape=}")
-        xcc2 = torch.cat([xup2, x_e32], dim=1)
-        if self.debug: print(f"{xcc2.shape=}")
-        xd21 = relu(self.decoder2_1(xcc2))
-        xd22 = relu(self.decoder2_2(xd21))
+        xup2 = self.upconv2(xd1)
+        xcc2 = torch.cat([xup2, x_e3], dim=1)
+        xd2 = self.decoder2(xcc2)
 
-        xup3 = self.upconv3(xd22)
-        xcc3 = torch.cat([xup3, x_e22], dim=1)
-        xd31 = relu(self.decoder3_1(xcc3))
-        xd32 = relu(self.decoder3_2(xd31))
+        xup3 = self.upconv3(xd2)
+        xcc3 = torch.cat([xup3, x_e2], dim=1)
+        xd3 =  self.decoder3(xcc3) 
 
-        xup4 = self.upconv4(xd32)
-        xcc4 = torch.cat([xup4, x_e12], dim=1)
-        xd41 = relu(self.decoder4_1(xcc4))
-        xd42 = relu(self.decoder4_2(xd41))
-
-        output = self.seg_out(xd42)
+        xup4 = self.upconv4(xd3)
+        xcc4 = torch.cat([xup4, x_e1], dim=1)
+        xd4 = self.decoder4(xcc4)
+        output = self.seg_out(xd4)
         #clamp to prob 
         if(self.number_classes == 1):
             if self.debug: print("output")
